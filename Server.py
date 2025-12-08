f38192747593884a0dc5d44d1077dfca5ddc2e15
sonyalapina <crolicaa@gmail.com>
2025-12-08 20:54:38 +0300

    final version

---
+++ Server.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+import os
+import sys
+import errno
+import time
+
+def server():
+    #общий файл
+    shared_file = "/tmp/shared_communication.txt"
+    
+    print("\nСервер запущен и ждет запрос...")
+    
+    try:
+        #создаем файл если не существует
+        if not os.path.exists(shared_file):
+            with open(shared_file, 'w') as f:
+                pass
+            print(f"Создан общий файл: {shared_file}")
+        
+        while True:
+            try:
+                fd = os.open(shared_file, os.O_RDWR)
+                
+                try:
+                    
+                    os.lockf(fd, os.F_LOCK, 0)
+                    
+                    #перемещаем указатель в начало
+                    os.lseek(fd, 0, os.SEEK_SET)
+                    
+                    #читаем данные
+                    data = os.read(fd, 1024)
+                    
+                    if data:
+                        message = data.decode('utf-8').strip()
+                        print(f"Получено сообщение: '{message}'")
+                        
+                        #очищаем файл
+                        os.ftruncate(fd, 0)
+                        
+                        if message.lower() == "ping":
+                            response = "pong"
+                        else:
+                            response = "Ошибка: неверный запрос"
+                        
+                        #записываем ответ в файлик
+                        os.lseek(fd, 0, os.SEEK_SET)
+                        os.write(fd, response.encode('utf-8'))
+                        
+                        #сбрасываем буферы на диск
+                        os.fsync(fd)
+                        
+                        print("\n")
+                    
+                    #снимаем блокировку
+                    os.lockf(fd, os.F_ULOCK, 0)
+                    
+                except Exception as e:
+                    #ошибка - снимаем блокировку
+                    try:
+                        os.lockf(fd, os.F_ULOCK, 0)
+                    except:
+                        pass
+                    raise
+                
+                os.close(fd)
+               
+                    
+            except KeyboardInterrupt:
+                print("\nСервер завершает работу...")
+                break
+            except Exception as e:
+                print(f"Ошибка: {e}")
+                continue
+                
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        return 1
+    finally:
+        #удаляем файл при завершении
+        try:
+            os.unlink(shared_file)
+            print(f"Файл {shared_file} удален")
+        except:
+            pass
+    
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(server())
